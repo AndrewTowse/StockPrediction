7f1f79c8012fe8b572e8d83649e3aad604086bf8
AndrewTowse <a15tows@siena.edu>
2020-11-12 17:42:02 -0500

    Changed the network to concatenate four models, 1 with the ohlc information and 3 with the technical indicators.

--- Prediction/neuralNetwork.py
+++ Prediction/neuralNetwork.py
@@ -6,7 +6,7 @@ import pandas as pd
 import manipulate_data_set as mp
 
 csvPath = r"C:\Users\andre\PycharmProjects\StockPrediction\Prediction\AAPL.csv"
-ohlcArray, technicIndicatorsNorm, nextDayOpenNorm, nextDayOpen, openNormaliser = mp.createDataSet(csvPath)
+ohlcArray, smaIndicators, macdIndicators, emaIndicators, nextDayOpenNorm, nextDayOpen, openNormaliser = mp.createDataSet(csvPath, 25)
 '''
 print("OHLC SHAPE: ", ohlcArray.shape)
 print("Indicator SHAPE: ", technicIndicatorsNorm.shape)
@@ -29,24 +29,32 @@ trainSize = int(ohlcArray.shape[0] * trainSize)
 #seperating the data into respective sizes
 #Training Data
 ohlcTrain = ohlcArray[:trainSize]
-technicTrain = technicIndicatorsNorm[:trainSize]
+smaTrain = smaIndicators[:trainSize]
+emaTrain = emaIndicators[:trainSize]
+macdTrain = macdIndicators[:trainSize]
 nextNormTrain = nextDayOpenNorm[:trainSize]
 nextOpenTrain = nextDayOpen[:trainSize]
 
 #Test Data
 ohlcTest = ohlcArray[trainSize:]
-technicTest = technicIndicatorsNorm[trainSize:]
+smaTest = smaIndicators[trainSize:]
+emaTest = emaIndicators[trainSize:]
+macdTest = macdIndicators[trainSize:]
 nextNormTest = nextDayOpenNorm[trainSize:]
 nextOpenTest = nextDayOpen[trainSize:]
 
 
 #prints shape of all the data
 print("Ohlc SHAPE: ", ohlcTrain.shape)
-print("Indicator SHAPE: ", technicTrain.shape)
+print("SMA Indicator SHAPE: ", smaTrain.shape)
+print("EMA Indicator SHAPE: ", emaTrain.shape)
+print("MACD Indicator SHAPE: ", macdTrain.shape)
 print("NextDayOpenNormalised SHAPE: ", nextNormTrain.shape)
 print("NextDayOpenOriginal SHAPE: ", nextOpenTrain.shape)
 
 
+
+'''
 #Making two seperate models will allow us to have ohlc data not affect the techIndicators until the final model
 #Model that will use the LSTM
 ohlcInput = layers.Input(shape=(ohlcTrain.shape[1], 5))
@@ -57,41 +65,69 @@ ohlcLSTMLayer = layers.Dropout(rate=0.2)(ohlcLSTMLayer)
 #creates first model to be concatenated
 ohlcModel = keras.Model(inputs=ohlcInput, outputs=ohlcLSTMLayer)
 
-
-#Model for the technical indicators
-technicInput = layers.Input(shape= (technicTrain.shape[1],))
+################## SMA ###################
+#Model for the sma indicators
+smaInput = layers.Input(shape= (smaTrain.shape[1],))
 #dense layer for technical side
-technicLayer = layers.Dense(20)(technicInput)
+smaLayer = layers.Dense(20)(smaInput)
 #
-technicLayer = layers.Activation("relu")(technicLayer)
+smaLayer = layers.Activation("relu")(smaLayer)
 #technicLayer = layers.Activation("relu")(technicLayer)
 #dropout layer to prevent overfitting
-technicLayer = layers.Dropout(rate=0.2)(technicLayer)
+smaLayer = layers.Dropout(rate=0.2)(smaLayer)
 #creates second model for the technicIndicator
-technicModel = keras.Model(inputs=technicInput, outputs=technicLayer)
+smaModel = keras.Model(inputs=smaInput, outputs=smaLayer)
+########################################
 
+################## EMA ###################
+#Model for the sma indicators
+emaInput = layers.Input(shape= (emaTrain.shape[1],))
+#dense layer for technical side
+emaLayer = layers.Dense(20)(emaInput)
+#
+emaLayer = layers.Activation("relu")(emaLayer)
+#technicLayer = layers.Activation("relu")(technicLayer)
+#dropout layer to prevent overfitting
+emaLayer = layers.Dropout(rate=0.2)(emaLayer)
+#creates second model for the technicIndicator
+emaModel = keras.Model(inputs=emaInput, outputs=emaLayer)
+########################################
 
-mergedModel = layers.concatenate([ohlcModel.output, technicModel.output])
+################## MACD ###################
+#Model for the sma indicators
+macdInput = layers.Input(shape= (macdTrain.shape[1],))
+#dense layer for technical side
+macdLayer = layers.Dense(20)(macdInput)
+#
+macdLayer = layers.Activation("relu")(macdLayer)
+#technicLayer = layers.Activation("relu")(technicLayer)
+#dropout layer to prevent overfitting
+macdLayer = layers.Dropout(rate=0.2)(macdLayer)
+#creates second model for the technicIndicator
+macdModel = keras.Model(inputs=macdInput, outputs=macdLayer)
+########################################
+
+mergedModel = layers.concatenate([ohlcModel.output, smaModel.output, emaModel.output, macdModel.input])
 #uses sigmoid activation to make sure the outputs are in between 0 and 1
-mergedModel = layers.Dense(64, activation= 'sigmoid')(mergedModel)
+mergedModel = layers.Dense(128, activation= 'sigmoid')(mergedModel)
 mergedModel = layers.Activation('relu')(mergedModel)
 #mergedModel = layers.Dropout(rate=0.2)(mergedModel)
 #mergedModel = layers.Dense(64, activation= 'sigmoid')(mergedModel)
 mergedModel = layers.Dense(1, activation= 'linear')(mergedModel)
 
-finalModel = keras.Model(inputs=[ohlcInput, technicInput], outputs=mergedModel)
+finalModel = keras.Model(inputs=[ohlcInput, smaInput, emaInput, macdInput], outputs=mergedModel)
 
 adam = keras.optimizers.Adam(lr=0.0005)
 finalModel.compile(optimizer= adam, loss="mse")
-finalModel.fit([ohlcTrain, technicTrain], nextNormTrain, epochs=100, batch_size=32, shuffle= True, verbose=1)
+finalModel.fit([ohlcTrain, smaTrain, emaTrain, macdTrain], nextNormTrain, epochs=100, batch_size=32, shuffle= True, verbose=1)
 
 
+finalModel.save(f"finalChangingModel.h5")
 
+'''
+finalModel = keras.models.load_model("finalSavedModel.h5")
 
-
-finalModel.save(f"finalSavedModel.h5")
-
-predicted = finalModel.predict([ohlcTest, technicTest])
+predicted = finalModel.predict([ohlcTest, smaTest, emaTest, macdTest])
 
 #print(predicted)
 predicted = openNormaliser.inverse_transform(predicted)
@@ -99,9 +135,13 @@ predicted = openNormaliser.inverse_transform(predicted)
 #assert nextDayOpen.shape == predicted.shape, "They be different shapes"
 
 
+
+'''
 realMeanSquaredError = np.mean(np.square(nextOpenTest - predicted))
+print("Real mean error: ", realMeanSquaredError)
 scaledMeanSquaredError = realMeanSquaredError / (np.max(nextOpenTest) - np.min(nextOpenTest)) * 100
-print(scaledMeanSquaredError)
+print("Scaled mean error: ", scaledMeanSquaredError)
+'''
 
 
 
@@ -112,7 +152,7 @@ plt.gcf().set_size_inches(22, 15, forward=True)
 start = 0
 end = -1
 
-real = plt.plot(nextOpenTest[start:end],'o', label='real')
+real = plt.plot(nextOpenTest[start:end], label='real')
 pred = plt.plot(predicted[start:end],"o", label='predicted')
 
 '''
