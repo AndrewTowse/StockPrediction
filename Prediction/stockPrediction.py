46db74081c25c2f4230019dcaa828e88934d3b12
AndrewTowse <a15tows@siena.edu>
2020-11-13 14:41:10 -0500

    Began stockPrediction file that will be calling prediction methods for automation

---
+++ Prediction/stockPrediction.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import numpy as np
+import pandas as pd
+import manipulate_data_set as mp
+import matplotlib.pyplot as plt
+import predictionMethods as pm
+
+path = "finalSavedModel.h5"
+
+#print(pm.retrieve_ohlcData())
+pm.retrieve_ohlcData()
+
+
+
+
